8b04a2bc5c67e9cdd70af1dbfae0a6ef9bf5c424
hynavera-pc <thanhhuyenah@gmail.com>
2025-01-10 18:17:55 +0700

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import "./Home.css";
+import rvImg1 from "../assets/u1.jpg"
+import rvImg2 from "../assets/u2.jpg"
+import rvImg3 from "../assets/u3.jpg"
+import rvImg4 from "../assets/u4.jpg"
+import { HiOutlineChevronDown, HiOutlineChevronUp } from "react-icons/hi";
+
+const testimonials = [
+  {name: "Nguyen, Jane", review: "I had the crepe filled with chestnut butter and it was like tasting heaven.", img: rvImg1},
+  {name: "Esther Howard", review: "I had the crepe filled with chestnut butter and it was like tasting heaven.", img: rvImg2},
+  {name: "Jerome Bell", review: "All crepe selections, plenty of variety for a meal or a dessert! ", img: rvImg3},
+  {name: "Jenny Wilson", review: "This place took it to a whole new level. And I love it <3", img: rvImg4},
+]
 
 const Home = () => {
   return (
     <div className=''>
-      <div className="hero wide">
+      <section className="hero wide">
         <div className="row">
           <div className="col l-6 hero-img">
             <img src="https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh3.png?alt=media&token=0194ba1b-8e24-4035-8870-111234834154" alt="" />
@@ -27,8 +39,8 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="about">
+      </section>
+      <section className="about">
         <div className="wide">
           <div className="row">
             <div className="col l-0-1 l-7">
@@ -43,7 +55,94 @@ const Home = () => {
         <div className="about-img">
           <img src="https://cdn.pixabay.com/photo/2025/01/10/08/05/08-05-31-202_960_720.jpg" alt="" />
         </div>
-      </div>
+      </section>
+      <section className="order">
+        <div className="wide">
+        <div className="row order-row">
+          <div className="order-img">
+            <img src="https://cdn.pixabay.com/photo/2025/01/10/09/07/09-07-08-569_960_720.png" alt="" />
+          </div>
+          <div className="col l-0-7 l-4 hero-menu">
+            <h4>ORDER ONLINE</h4>
+            <h1>“Paris-outdoors to your home”</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consecte tur adipis cing elit, sed do eiusmod tempor incididu nt  Lorem ipsum dolor sit amet, consecte tur adipiscing elit, sed do eiusm od.
+            </p>
+            <button className='btn1'>Order Now</button>
+          </div>
+        </div>
+        <div className="row">
+          <h3 className='col l-12 order-testi'>DINERS SAY ABOUT US</h3>
+          <div className='testi-layout row'>
+            {testimonials.map(review => (
+            <div className="testi-card col l-4" key={review.name}>
+                <img src={review.img} className='testi-img'/>
+                <div>
+                  <div className="testi-star">
+                  <div><p className='p1'>{review.name}</p></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
+                  </div>
+                  <div><p className='p0'>{review.review}</p></div>
+                </div>
+            </div>
+            ))}
+          </div>
+          </div>
+        </div>
+      </section>
+      <section className="events">
+        <div className="wide">
+          <div className="row order-row">
+            <div className="col l-0-1 l-4 hero-menu">
+              <h4>EVENTS</h4>
+              <h1>“Dinning through the night!”</h1>
+              <p>
+              Lorem ipsum dolor sit amet, consecte tur adipis cing elit, sed do eiusmod tempor incididu nt  Lorem ipsum dolor sit amet, consecte tur adipiscing elit, sed do eiusm od.
+              </p>
+              <button className='btn2'>Book a Table</button>
+            </div>
+            <div className="order-img">
+              <img src="https://cdn.pixabay.com/photo/2025/01/10/10/51/10-51-19-802_960_720.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="faq wide">
+        <div className="row faq-row">
+          <div className="col l-5">
+            <h3>FAQ</h3>
+            <div className="faq-layout">
+              <div className="faq-qt row">
+                <p>Where do you deliver?</p>
+                <HiOutlineChevronDown/>
+              </div>
+              <div className="faq-qt row">
+                <p>How long does it take to receive my food order?</p>
+                <HiOutlineChevronDown/>
+              </div>
+              <div className="faq-qt row">
+                <p>How much does the delivery cost?</p>
+                <HiOutlineChevronDown/>
+              </div>
+              <div className="faq-qt row">
+                <p>How much does the reservation cost?</p>
+                <HiOutlineChevronDown/>
+              </div>
+              <div className="faq-qt row">
+                <p>Who can I contact with questions?</p>
+                <HiOutlineChevronDown/>
+              </div>
+            </div>
+          </div>
+          <div className="col l-5">
+            <h3>VISIT US</h3>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }

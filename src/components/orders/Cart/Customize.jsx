5df4774bd8e8c0f99c8b7c11050d6e9d995f8167
hynavera-pc <thanhhuyenah@gmail.com>
2025-01-15 18:58:55 +0700

    fix

--- src/components/orders/Cart/Customize.jsx
+++ src/components/orders/Cart/Customize.jsx
@@ -1,26 +1,81 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-// import { removeItemFromCart, increaseItemQuantity, decreaseItemQuantity, clearCart } from './cartSlice';
+import { addItemToCart, removeItemFromCart } from './cartSlice';
+// import { clearCart } from './cartSlice';
 import Popup from 'reactjs-popup';
 import DishItem from './DishItem';
 
 
 const Customize = () => {
   const cartItems = useSelector(state => state.cart.cartItems);
-
-  // const dispatch = useDispatch();
-  // const handleRemove = (dish) => {
-  //   dispatch(removeItemFromCart(dish));
-  // };
-  // const handleIncrement = (item) => {
-  //   dispatch(increaseItemQuantity(item));
-  // };
-  // const handleDecrement = (item) => {
-  //  dispatch(decreaseItemQuantity(item));
-  // };
+  
+  const dispatch = useDispatch();
+  const handleAddToCart = (dish) => {
+    dispatch(addItemToCart(dish));
+    console.log(dish.name);
+  };
+  const handleRemoveFromCart = (dish) => {
+    dispatch(removeItemFromCart(dish));
+    console.log(dish, "remove");
+  }
   // const handleClearCart = () => {
   //   dispatch(clearCart());
   // };
+
+  const [dishes, setDishes] = useState([]);
+  // const [randomDish, setRandomDish] = useState({
+  //   nameRd: "", imageRd: "", aboutRd: "", priceRd: ""
+  // })
+  const [randomDishes, setRandomDishes] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.npoint.io/cbab319f8372ce70638e');
+        const data = await response.json();
+
+        if (data) {
+          const filteredDishes = data.filter(
+            (item) => item.category === 'Savory Galettes' || item.category === 'Sweet Crepes'
+          );
+          setDishes(filteredDishes.map((item) => item.crepes)); // Extract only the 'crepes' arrays
+        } else {
+          console.error('Error fetching data or invalid data structure.');
+        }
+      } catch (error) {
+        console.error('Error fetching dishes:', error);
+      }
+    };
+    fetchData();
+  }, []);  
+
+  useEffect(() => {
+    if (dishes.length > 0) {
+      // getRandom(); 
+      getRandomDishes();
+    }
+  }, [dishes]); 
+
+    // const getRandom = () => {
+    //   if (dishes.length === 0) return; // Exit early if dishes is empty
+    //   const randomDishIndex = Math.floor(Math.random() * dishes.flat().length); // Combine all crepes arrays
+    //   const selectedDish = dishes.flat()[randomDishIndex]; // Select a random dish from the combined array   
+    //   setRandomDish({
+    //     nameRd: selectedDish.name,
+    //     imageRd: selectedDish.image,
+    //     aboutRd: selectedDish.about,
+    //     priceRd: selectedDish.price,
+    //   });
+    // };  
+    const getRandomDishes = () => {
+      const allCrepes = dishes.flat(); 
+      const uniqueIndices = new Set(); 
+      while (uniqueIndices.size < 6) {
+        uniqueIndices.add(Math.floor(Math.random() * allCrepes.length));
+      }
+      const selectedDishes = Array.from(uniqueIndices).map((index) => allCrepes[index]);
+      setRandomDishes(selectedDishes);
+    };
 
   return (
     <div className='order-wide'>
@@ -50,7 +105,8 @@ const Customize = () => {
                   <div className="dprice">{item.price}</div>
                 </div>
                 <div className="add-btn">
-                <Popup trigger={
+                <Popup
+                  trigger={
                     <button className="btn2">
                       x {item.quantity}
                     </button>
@@ -61,18 +117,62 @@ const Customize = () => {
                   </div>
                 </Popup>
                 </div>
-                {/* <div className="add-btn">
-                  <div className="row">
-                    <button onClick={() => handleDecrement(item.name)}>-</button>
-                    <button onClick={() => handleIncrement(item.name)}>+</button>
-                    <button onClick={() => handleRemove(item.name)}>Remove</button>
-                  </div>
-                </div> */}
               </div>
             </div>
           </div>
           </div>
           ))}
+        </div>
+        </div>
+      </section>
+      <hr />
+      <section className="crepe-layout">
+        <div className="crepe-type">
+          <div className="row">
+            <div className="col l-12">
+              <h3>Other Suggestion</h3>
+            </div>
+          </div>
+        <div className="row dish-row">
+              {/* <div className="dish-name">
+                <div className='dname'>{randomDish.nameRd}</div>
+                <p className="p0 dabout">{randomDish.aboutRd}</p>
+              </div> */}
+        {randomDishes.map((dish, index) => (
+        <div key={index} className="col l-3 dish-card-lt">
+          <div className="dish-card">
+            <div className="card-linear"></div>
+            <div className="dimage">
+              <img src={dish.image} alt="" />
+            </div>
+            <div className="dish-info">
+              <div className="dish-name">
+                <div className='dname'>{dish.name}</div>
+                <p className="p0 dabout">{dish.about}</p>
+              </div>
+              <div className="row dish-add">
+                <div className="row dish-price">
+                  <p className="p1">$</p>
+                  <div className="dprice">{dish.price}</div>
+                </div>
+                <div className="add-btn">
+                  {cartItems.some((item) => item.name === dish.name) ? (
+                    <button className='btn2' onClick={() => handleRemoveFromCart(dish.name)}>Remove</button>
+                  ) : (
+                    <button className='btn1' onClick={() => handleAddToCart(dish)}>Add</button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        ))}
+        </div>
+        <br />
+        <div className="row">
+          <div className="col l-3">
+            <button className="btn1">View All Menu</button>
+          </div>
         </div>
         </div>
       </section>
--- src/components/orders/Cart/DishItem.jsx
+++ src/components/orders/Cart/DishItem.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./DishPopup.css"
 import { useSelector, useDispatch } from 'react-redux';
-import { increaseItemQuantity, decreaseItemQuantity} from './cartSlice';
+import { increaseItemQuantity, decreaseItemQuantity, removeItemFromCart } from './cartSlice';
 import Cart from './Cart';
 
 const DishItem = ({name, image, about, price, quantity}) => {
   const dispatch = useDispatch();
+  
   const handleIncrement = (item) => {
     dispatch(increaseItemQuantity(item));
   };
   const handleDecrement = (item) => {
    dispatch(decreaseItemQuantity(item));
+  };
+  const handleRemove = (item) => {
+    dispatch(removeItemFromCart(item));
   };
 
   return (
@@ -19,37 +23,42 @@ const DishItem = ({name, image, about, price, quantity}) => {
         <div className="row cart-popup">
           <Cart/>
         </div>
-        <div className="row">
+        <div className="row pdish">
           <div className="col l-5">
             <div className="pimage">
               <img src={image} alt="" />
             </div>
           </div>
           <div className="col l-5 pinfo">
-            <h2>{name}</h2>
-            <p>{about}</p>
+            <div>
+              <h2>{name}</h2>
+              <p>{about}</p>
+            </div>
             <div className="row pquantity">
               <h4>Quantity:</h4>
               <div className="row">
-                <button onClick={() => handleDecrement(name)}>-</button>
-                <div>{quantity}</div>
-                <button onClick={() => handleIncrement(name)}>+</button>
+                <button className='btn1' onClick={() => handleDecrement(name)}>-</button>
+                <h4>{quantity}</h4>
+                <button className='btn1' onClick={() => handleIncrement(name)}>+</button>
               </div>
             </div>
             <div className="padd">
               <h4>Add more:</h4>
-              <div className="row">
+              <label>
                 <input type="checkbox" name="dress" id="" />
-                <label htmlFor="">Salad dressing</label>
-              </div>
-              <div className="row">
+                <span htmlFor="">Salad dressing</span>
+              </label>
+              <label>
                 <input type="checkbox" name="sauce" id="" />
-                <label htmlFor="">Hot sauce</label>
-              </div>
-              <div className="row">
+                <span htmlFor="">Hot sauce</span>
+              </label>
+              <label>
                 <input type="checkbox" name="cream" id="" />
-                <label htmlFor="">Mix cheese/whipped cream</label>
-              </div>
+                <span htmlFor="">Mix cheese/whipped cream</span>
+              </label>
+            </div>
+            <div className='premove'>
+              <button className="btn2" onClick={() => handleRemove(name)}>Remove</button>
             </div>
           </div>
           <div className="col l-2">
@@ -58,6 +67,9 @@ const DishItem = ({name, image, about, price, quantity}) => {
               <h4>{price}</h4>
             </div>
           </div>
+        </div>
+        <div className="row" style={{justifyContent: "flex-end"}}>
+          <p className='p0'>Click outside to continue</p>
         </div>
       </div>
       

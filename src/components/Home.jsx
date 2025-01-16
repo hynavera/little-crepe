39663e2ddeb550ab31491eda88bfc553610c06d6
hynavera-pc <thanhhuyenah@gmail.com>
2025-01-16 23:48:03 +0700

    ok web ver

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,13 @@
 import React, {useState} from 'react';
 import "./Home.css";
-import rvImg1 from "../assets/u1.png"
-import rvImg2 from "../assets/u2.png"
-import rvImg3 from "../assets/u3.png"
-import rvImg4 from "../assets/u4.png"
-import { HiOutlineChevronDown, HiOutlineChevronUp } from "react-icons/hi";
-import CustomMap from './CustomMap';
+import Hero from './landing/Hero';
+import Review from './landing/Review';
+import Faq from './landing/Faq';
+import CustomMap from './landing/CustomMap';
 import { APIProvider } from "@vis.gl/react-google-maps";
 import { useNavigate, Outlet } from 'react-router-dom'; 
 import { CgClose } from "react-icons/cg";
 
-
-const testimonials = [
-  {name: "Nguyen, Jane", review: "I had the crepe filled with chestnut butter and it was like tasting heaven.", img: rvImg1},
-  {name: "Esther Howard", review: "I had the crepe filled with chestnut butter and it was like tasting heaven.", img: rvImg2},
-  {name: "Jerome Bell", review: "All crepe selections, plenty of variety for a meal or a dessert! ", img: rvImg3},
-  {name: "Jenny Wilson", review: "This place took it to a whole new level. And I love it <3", img: rvImg4},
-]
 
 const Home = () => {
   const [openPopup, setOpenPopup] = useState(false);
@@ -32,30 +23,7 @@ const Home = () => {
   
   return (
     <div className=''>
-      <section className="hero wide">
-        <div className="row">
-          <div className="col l-6 hero-img">
-            <img src="https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh3.png?alt=media&token=0194ba1b-8e24-4035-8870-111234834154" alt="" />
-          </div>
-          <div className="col l-0-1 l-4 hero-menu">
-            <h4>CREPES DAY</h4>
-            <h1>Savory galettes <br />& sweets</h1>
-            <p>Enjoy unique crepes based on French recipes at a classic restaurant on the banks of the Red River, right in the heart of the capital Hanoi.
-            </p>
-            <div className="row hero-circle-row">
-              <div className="hero-circle">
-                <img src="https://images.unsplash.com/photo-1667902687249-0dfe266a3b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-              </div>
-              <div className="hero-circle">
-                <img src="https://images.unsplash.com/photo-1609501885647-9cf9deffdb23?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-              </div>
-              <div className="hero-circle">
-                <img src="https://images.unsplash.com/photo-1582995570162-9dee25247fda?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Hero/>
       <section className="about">
         <div className="wide">
           <div className="row">
@@ -78,24 +46,7 @@ const Home = () => {
         <div className="wide">
         <div className="row">
           <h3 className='col l-12 order-testi'>DINERS SAY ABOUT US</h3>
-          <div className='testi-layout row'>
-            {testimonials.map(review => (
-            <div className="testi-card col l-4" key={review.name}>
-                <img src={review.img} className='testi-img'/>
-                <div>
-                  <div className="testi-star">
-                  <div><p className='p1'>{review.name}</p></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>            
-                  </div>
-                  <div><p className='p0'>{review.review}</p></div>
-                </div>
-            </div>
-            ))}
-          </div>
+          <Review/>
           </div>
         <div className="row order-row">
           <div className="order-img">
@@ -151,44 +102,7 @@ const Home = () => {
             <h3>FAQ</h3>
             <p className='p0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             </p>
-            <br />
-            <div className="faq-layout">
-              <div className="faq-qt row">
-                <p className='ques'>Where do you deliver?</p>
-                <p className='ans'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                </p>
-                <HiOutlineChevronDown className='arr'/>
-              </div>
-              <div className="faq-qt row">
-                <p className='ques'>How long does it take to receive my food order?</p>
-                <p className='ans'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                </p>
-                <HiOutlineChevronDown className='arr'/>
-              </div>
-              <div className="faq-qt row">
-                <p className='ques'>How much does the delivery cost?</p>
-                <p className='ans'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                </p>
-                <HiOutlineChevronDown className='arr'/>
-              </div>
-              <div className="faq-qt row">
-                <p className='ques'>How much does the reservation cost?</p>
-                <p className='ans'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                </p>
-                <HiOutlineChevronDown className='arr'/>
-              </div>
-              <div className="faq-qt row">
-                <p className='ques'>Who can I contact with questions?</p>
-                <p className='ans'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                </p>
-                <HiOutlineChevronDown className='arr'/>
-              </div>
-            </div>
+            <Faq/>
           </div>
         </div>    
         <div className="row">
@@ -196,7 +110,6 @@ const Home = () => {
             <h3>VISIT US</h3>
             <p className='p0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             </p>
-            <br />
             <div className="map">
               <APIProvider apiKey={'AIzaSyDTBdN8scOp2wngX5Eim0IbtpzYHgIUABY'} onLoad={() => console.log('Maps API has loaded.')}>
                 <CustomMap />
---
+++ src/components/landing/Hero.jsx
@@ -0,0 +1,115 @@
+import React, {useEffect, useState, useRef} from 'react'
+
+const Hero = () => {
+  const [menu, setMenu] = useState(1);
+  const handleSetMenu1 = () => {
+    setMenu(1);
+  }
+  const handleSetMenu2 = () => {
+    setMenu(2);
+  }
+  const handleSetMenu3 = () => {
+    setMenu(3);
+  }
+
+  const [currentMenu, setCurrentMenu] = useState(1);
+  const prevMenuRef = useRef(1); // Use useRef to track the previous menu
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentMenu((prevMenu) => {
+        if (prevMenu === 3) {
+          return 1;
+        } else {
+          return prevMenu + 1;
+        }
+      });
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (currentMenu !== prevMenuRef.current) {
+      prevMenuRef.current = currentMenu;
+
+      if (imgRef.current) {
+        imgRef.current.classList.remove('hero-img-animation-1', 'hero-img-animation-2', 'hero-img-animation-3', 'active'); // Remove all classes
+        imgRef.current.classList.add(`hero-img-animation-${currentMenu}`); 
+      }
+
+      setTimeout(() => {
+        if (imgRef.current) {
+          imgRef.current.classList.add('active'); 
+        }
+        setMenu(currentMenu); 
+      }, 500); 
+    }
+  }, [currentMenu]);
+
+  return (
+    <section className="hero wide">
+      <div className="row">
+        <div className="col l-6 hero-img">
+          <img 
+            src={menu === 1 
+              ? "https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh1.png?alt=media&token=a117758e-b056-444b-b09a-c2746ca7ea84" 
+              : menu === 2 
+                ? "https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh2.png?alt=media&token=740d610e-85d0-4a35-a5ab-9f5bbb90667a" 
+                : "https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh3.png?alt=media&token=0194ba1b-8e24-4035-8870-111234834154" 
+            } 
+            ref={imgRef}
+          />
+          {/* {menu === 1 &&
+            <img src="https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh1.png?alt=media&token=a117758e-b056-444b-b09a-c2746ca7ea84" alt="" />
+          }
+          {menu === 2 &&
+            <img src="https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh2.png?alt=media&token=740d610e-85d0-4a35-a5ab-9f5bbb90667a" alt="" />
+          }
+          {menu === 3 &&
+            <img src="https://firebasestorage.googleapis.com/v0/b/huyennguyen-dev.appspot.com/o/Coding%2FQuatre-Crepes%2FProperty%201%3Dh3.png?alt=media&token=0194ba1b-8e24-4035-8870-111234834154" alt="" />
+          } */}
+        </div>
+        <div className="col l-0-1 l-4 hero-menu">
+          {menu === 1 && <>
+            <h4>CREPES DAY</h4>
+            <h1>Savory galettes <br />& sweets</h1>
+            <p>Enjoy unique crepes based on French recipes at a classic restaurant on the banks of the Red River, right in the heart of the capital Hanoi.
+            </p>
+          </>}
+          {menu === 2 && <>
+            <h4>SPECIALS-DAY</h4>
+            <h1>Traditional <br />des bonnes crêpes</h1>
+            <p>Enjoy unique crepes based on French recipes at a classic restaurant on the banks of the Red River, right in the heart of the capital Hanoi.
+            </p>
+          </>}
+          {menu === 3 && <>
+            <h4>FULL-TOPPINGS</h4>
+            <h1>Spices, mixes <br />& more dressings</h1>
+            <p>Enjoy unique crepes based on French recipes at a classic restaurant on the banks of the Red River, right in the heart of the capital Hanoi.
+            </p>
+          </>}
+          <div className="row hero-circle-row">
+            <div className="hero-circle" onClick={handleSetMenu1}>
+              <img src="https://images.unsplash.com/photo-1667902687249-0dfe266a3b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" 
+                className={`herobtn ${menu === 1 ? 'active' : ''}`}
+              />
+            </div>
+            <div className="hero-circle" onClick={handleSetMenu2}>
+              <img src="https://images.unsplash.com/photo-1609501885647-9cf9deffdb23?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" 
+                className={`herobtn ${menu === 2 ? 'active' : ''}`}
+              />
+            </div>
+            <div className="hero-circle" onClick={handleSetMenu3}>
+              <img src="https://images.unsplash.com/photo-1582995570162-9dee25247fda?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" 
+                className={`herobtn ${menu === 3 ? 'active' : ''}`}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
